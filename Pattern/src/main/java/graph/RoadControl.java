342d29988794161bd243e832ccdcbc9761ceaa60
Mohammad Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2024-08-19 19:05:11 +0330

    Add RoadControl class template

---
+++ Pattern/src/main/java/graph/RoadControl.java
@@ -0,0 +1,21 @@
+package graph;
+
+public class RoadControl {
+    private State state;
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public void makeAllRoutesOneWay(Graph graph) {
+        //TODO
+    }
+
+    public void makeAllRoutesTwoWay(Graph graph) {
+        //TODO
+    }
+
+    public void changeTrainUnitTime(Graph graph, int unit) {
+        //TODO
+    }
+}
