0cdede970275b0203b2451276dac06f12b2f5624
Mohammad Mahdi Vahedi <m.m.vahedi13800@gmail.com>
2024-08-19 18:28:45 +0330

    Add State pattern files

---
+++ Pattern/src/main/java/graph/OneWayState.java
@@ -0,0 +1,18 @@
+package graph;
+
+public class OneWayState implements State {
+    @Override
+    public void makeOneWay(Graph graph) {
+        //TODO
+    }
+
+    @Override
+    public void makeTwoWay(Graph graph) {
+        // Empty Body
+    }
+
+    @Override
+    public void changeTrainUnitTime(Graph graph, int unit) {
+        //TODO
+    }
+}
---
+++ Pattern/src/main/java/graph/TwoWayState.java
@@ -0,0 +1,18 @@
+package graph;
+
+public class TwoWayState implements State {
+    @Override
+    public void makeOneWay(Graph graph) {
+        //TODO
+    }
+
+    @Override
+    public void makeTwoWay(Graph graph) {
+        //TODO
+    }
+
+    @Override
+    public void changeTrainUnitTime(Graph graph, int unit) {
+        //TODO
+    }
+}
